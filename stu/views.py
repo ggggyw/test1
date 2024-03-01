7b9ba3ddd17478dd027f79f3cffe6c4a8ae795bb
Dai Zihan <18910497489@163.com>
2024-03-01 16:53:34 +0800

    Update views.py

--- stu/views.py
+++ stu/views.py
@@ -3,10 +3,6 @@ from .models import User
 from django.http import HttpResponse
 # Create your views here.
 def login(request):
-    return render(request,'login.html')
-
-
-def login(request):
     if request.method == 'POST':
         username = request.POST['name']  # 注意字段名称
         password = request.POST['code']
@@ -31,3 +27,20 @@ def login(request):
             return render(request, 'login.html', {'error': '用户名不存在'})
 
     return render(request, 'login.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['name']
+        password = request.POST['code']
+        role = request.POST['role']
+
+        # Check if this username already exists.
+        if User.objects.filter(name=username).exists():
+            return render(request, 'registration.html', {'error': '用户名已经存在'})
+
+        # If not exists, create the user.
+        User.objects.create(name=username, code=password, role=role)
+        return redirect('/login/')
+
+    return render(request, 'registration.html')
