8fcf5012f2da2f1ebf7a33efa6dfed10d43541e5
778899dumingyang <3193946089@qq.com>
2024-03-01 21:33:46 +0800

    Delete urls.py

--- stu/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from stu import views as stu_views
-
-urlpatterns=[
-    path('user/login/', stu_views.login, name='login'),
-    path('user/register/', stu_views.register, name='register'),
-    path('', stu_views.home, name='home'),
-]
