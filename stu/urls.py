919e3e6a1cfef61eb2bcc1015f5d1fde48e5fbe6
Dai Zihan <18910497489@163.com>
2024-03-01 16:53:37 +0800

    Update urls.py

--- stu/urls.py
+++ stu/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from stu import views
 
 urlpatterns=[
-    path('',views.login)
+    path('',views.login),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
 ]
