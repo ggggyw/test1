0a9131eec9cca2a3b3a2b73130f797b75a255a6b
778899dumingyang <3193946089@qq.com>
2024-03-05 16:33:31 +0800

    Update urls.py

--- login_register/urls.py
+++ login_register/urls.py
@@ -4,5 +4,6 @@ from login_register import views as login_register_views
 urlpatterns=[
     path('user/login/', login_register_views.login, name='login'),
     path('user/register/', login_register_views.register, name='register'),
+    path('user/userspage/', login_register_views.userspage, name='userspage'),
     path('', login_register_views.home, name='home'),
 ]
