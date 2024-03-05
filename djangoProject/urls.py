3a758426770d91cc59de8b6afd17aa349ab72fac
778899dumingyang <3193946089@qq.com>
2024-03-05 16:07:33 +0800

    Update urls.py

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/register/', login_register_views.register, name='register'),
     path('user/login/', login_register_views.login, name='login'),
+    path('user/userspage/', login_register_views.userpage, name='login'),
     path('', login_register_views.home, name='home'),
 ]
