c8c6018d73a93ecec9f05a1cd9cf10f4d51eeb88
ggggyw <2326594384@qq.com>
2024-03-08 13:52:06 +0800

    Update views.py

--- djangoProject/views.py
+++ djangoProject/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from login_register.models import Product
 
 
 def home(request):
-    return render(request, '首页.html')
+    products = Product.objects.all()  # 获取所有商品对象
+    context = {'products': products}  # 构建上下文字典
+    return render(request, '首页.html',context)
