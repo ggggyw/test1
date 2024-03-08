3e6a8ce4390226e4cd45effae1023f70e9df0511
ggggyw <2326594384@qq.com>
2024-03-08 11:02:37 +0800

    Update views.py

--- user/views.py
+++ user/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from login_register.models import Product
 
 
 # Create your views here.
 def userspage(request):
-    return render(request, 'userpage.html')
+    products = Product.objects.all()  # 获取所有商品对象
+    context = {'products': products}  # 构建上下文字典
+    return render(request, 'userpage.html',context)
 
 def userprofile(request):
     return render(request, 'userprofile.html')
