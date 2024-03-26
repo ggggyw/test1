13ff6f0659953f2925a85ed96f97899c4f776dca
778899dumingyang <3193946089@qq.com>
2024-03-26 14:59:05 +0800

    Update views.py

--- login_register/views.py
+++ login_register/views.py
@@ -17,7 +17,7 @@ def login(request):
                 user = Users.objects.get(u_acc=user_acc)
                 if (user.u_acc == user_acc) & (user.u_psw == password):
                     user_id = user.u_id  # 获取用户ID
-                    return redirect(reverse('userspage', kwargs={'ID': user_id, 'role': role}))  #返回用户ID和角色
+                    return redirect(reverse('userpage', kwargs={'ID': user_id, 'role': role}))  #返回用户ID和角色
                 else:
                     return render(request, 'login.html', {'error': 'Invalid username, password, or role'})
             elif role == 'admin':
@@ -26,7 +26,7 @@ def login(request):
                     print(user)
                     if (user.ad_acc == user_acc) & (user.ad_psw == password):
                         admin_id = user.ad_id  # 获取管理员ID
-                        return redirect(reverse('userspage', kwargs={'ID': admin_id, 'role': role}))  # 返回管理员ID和角色
+                        return redirect(reverse('userpage', kwargs={'ID': admin_id, 'role': role}))  # 返回管理员ID和角色
                     else:
                         return render(request, 'login.html', {'error': 'Invalid username, password, or role'})
                 except Admin.DoesNotExist:
@@ -37,7 +37,7 @@ def login(request):
                     print(user)
                     if (user.s_acc == user_acc) & (user.s_psw == password):
                         shop_id = user.s_id   #获取商家ID
-                        return redirect(reverse('userspage', kwargs={'ID': shop_id, 'role': role}))  # 返回商家ID和角色
+                        return redirect(reverse('userpage', kwargs={'ID': shop_id, 'role': role}))  # 返回商家ID和角色
                     else:
                         return render(request, 'login.html', {'error': 'Invalid username, password, or role'})
                 except Shops.DoesNotExist:
