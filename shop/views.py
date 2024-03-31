ec50ad7612d87f0ee167e66237e5cb1d7ec58c71
778899dumingyang <3193946089@qq.com>
2024-03-31 22:51:11 +0800

    Update views.py

--- shop/views.py
+++ shop/views.py
@@ -12,7 +12,7 @@ def shoppage(request):
     # 从会话中获取用户的ID
     u_id = request.session.get('u_id')
     shop_products = ShopProducts.objects.filter(shop__s_id=u_id)
-    paginator = Paginator(shop_products, 24)  # 假设每页显示多少个商品
+    paginator = Paginator(shop_products, 3)  # 假设每页显示多少个商品
     products = Products.objects.all()
     page = request.GET.get('page')  # 从GET请求的查询参数中获取页码
     paged_products = paginator.get_page(page)  # 获取当前页的商品对象列表
@@ -45,7 +45,7 @@ def myproducts(request):
     products = Products.objects.all()
 
     # 创建一个 Paginator 对象，每页显示 24 个商品
-    paginator = Paginator(shop_products, 24)
+    paginator = Paginator(shop_products, 3)
     # 从 GET 请求的查询参数中获取页码
     page = request.GET.get('page')
     # 使用 Paginator 对象的 get_page 方法来获取当前页的商品
@@ -59,11 +59,12 @@ def myproducts(request):
         # 如果用户没有登录，就渲染 首页.html 模板
         template_name = '首页.html'
 
-    content = render_to_string(template_name, {'shop_products': shop_products, 'products': products, 'category_id': category_id})
+    content = render_to_string(template_name,
+                               {'shop_products': shop_products, 'products': products, 'category_id': category_id})
     return HttpResponse(content)
 
 
-def shop_productdetails(request , p_id):
+def shop_productdetails(request, p_id):
     u_id = request.session.get('u_id')
     role = request.session.get('role')
     shop_product = ShopProducts.objects.get(shop_product_id=p_id)
@@ -72,7 +73,31 @@ def shop_productdetails(request , p_id):
                   {'shop_product': shop_product, 'products': products, 'u_id': u_id, 'role': role})
 
 def manage_products(request):
-    return render(request, 'shop_manage_products.html')
+    # 从会话中获取用户的ID
+    u_id = request.session.get('u_id')
+    shop_products = ShopProducts.objects.filter(shop__s_id=u_id)
+    paginator = Paginator(shop_products, 3)  # 假设每页显示多少个商品
+    products = Products.objects.all()
+    page = request.GET.get('page')  # 从GET请求的查询参数中获取页码
+    paged_products = paginator.get_page(page)  # 获取当前页的商品对象列表
+
+    u_id = request.session.get('u_id')
+    role = request.session.get('role')
+
+    context = {
+        'shop_products': paged_products,
+        'products': products,
+        'user_id': u_id,
+    }
+    return render(request, 'shop_manage_products.html', context)
+
+
+def edit_product(request):
+    return None
+
+
+def delete_product(request):
+    return None
 
 
 def shop_order(request):
@@ -81,4 +106,3 @@ def shop_order(request):
 
 def sales_analysis(request):
     return render(request, 'shop_sales_analysis.html')
-
