17e5358134e190615bda1c617ebb9190ac096237
778899dumingyang <3193946089@qq.com>
2024-03-31 15:58:20 +0800

    Update urls.py

--- shop/urls.py
+++ shop/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('shoppage/', views.shoppage, name='shoppage'),
-    path('shoppage/get_products_fromshop/', views.get_products_fromshop, name='get_products_fromshop'),
     path('shoppage/myproducts/', views.myproducts, name='myproducts'),
     path('shoppage/manage_products/', views.manage_products, name='manage_products'),
     path('shoppage/shop_order/', views.shop_order, name='shop_order'),
