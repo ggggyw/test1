cf8fae084e4b2a3bf2128cafc23c192e0328dc35
778899dumingyang <3193946089@qq.com>
2024-04-01 11:18:44 +0800

    Update urls.py

--- shop/urls.py
+++ shop/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('shoppage/manage_products/', views.manage_products, name='manage_products'),
     path('shoppage/manage_products/edit_product/<int:product_id>/', views.edit_product, name='edit_product'),
     path('shoppage/manage_products/delete_product/<int:product_id>/', views.delete_product, name='delete_product'),
+    path('shoppage/manage_products/product_detail/<int:product_id>/', views.product_detail, name='delete_product'),
     path('shoppage/shop_order/', views.shop_order, name='shop_order'),
     path('shoppage/sales_analysis/', views.sales_analysis, name='sales_analysis'),
     path('shoppage/shop_productdetails/<int:p_id>/', views.shop_productdetails, name='shop_productdetails'),
