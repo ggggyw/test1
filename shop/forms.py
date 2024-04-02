f51196705bc10c9732a60f3183ae8fc160f57f50
778899dumingyang <3193946089@qq.com>
2024-04-02 16:58:22 +0800

    Update forms.py

--- shop/forms.py
+++ shop/forms.py
@@ -25,6 +25,17 @@ class ProductForm(forms.ModelForm):
 
 
 class ShopProductForm(forms.ModelForm):
+    # 定义商品状态的选项
+    STATUS_CHOICES = (
+        ('上架', '上架'),  # 第一个值是存储在数据库中的实际值，第二个值是在表单上显示的友好名称
+        ('下架', '下架')
+    )
+
+    product_status = forms.ChoiceField(
+        choices=STATUS_CHOICES,
+        label="商品状态",
+    )
+
     class Meta:
         model = ShopProducts
         fields = ['product_desc', 'product_status', 'stock_quantity',
@@ -32,10 +43,10 @@ class ShopProductForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(ShopProductForm, self).__init__(*args, **kwargs)
-        # 设置自定义标签
+        # 保留其他自定义标签设置
         self.fields['product_desc'].label = "商品描述"
-        self.fields['product_status'].label = "商品状态"
         self.fields['stock_quantity'].label = "库存数量"
         self.fields['original_price'].label = "原始价格"
         self.fields['discount'].label = "折扣"
         self.fields['current_price'].label = "现价"
+        # 商品状态的自定义标签已在上面通过ChoiceField设置
