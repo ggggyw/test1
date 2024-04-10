886df7a61f3de2958b36e518747aa902fe18e348
Dai Zihan <18910497489@163.com>
2024-04-10 22:49:25 +0800

    Update views.py

--- admin/views.py
+++ admin/views.py
@@ -10,10 +10,10 @@ def adminpage(request):
     admin = Admin.objects.get(ad_id=request.session['ad_id'])
     users = Users.objects.all()
     paginator = Paginator(users, 6)  # 每页显示 6 个用户
-    page_number = request.GET.get('page')
+    page_number = request.GET.get('user_page')
     page_obj = paginator.get_page(page_number)
     shops = Shops.objects.all()
-    shop_paginator = Paginator(shops, 3)  # 每页显示 6 个商店
+    shop_paginator = Paginator(shops, 6)  # 每页显示 6 个商店
     shop_page_number = request.GET.get('shop_page')
     shop_page_obj = shop_paginator.get_page(shop_page_number)
     context = {
