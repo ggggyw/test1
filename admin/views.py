ead658eadd6f5ce2effaa355cd45ec95972fc666
Dai Zihan <18910497489@163.com>
2024-04-10 18:50:58 +0800

    管理员界面

--- admin/views.py
+++ admin/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from common.models import Admin
+from common.models import Admin, Users,Shops,ShopProducts,Orders,OrderDetails,Products
+from django.core.paginator import Paginator
 
 def adminpage(request):
     if 'ad_id' not in request.session:
         return redirect('login')
 
     admin = Admin.objects.get(ad_id=request.session['ad_id'])
+    users = Users.objects.all()
+    paginator = Paginator(users, 6)  # 每页显示 6 个用户
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
         'admin': admin,
-        'is_super': admin.is_super
+        'is_super': admin.is_super,
+        'users': page_obj,
+        'shops': Shops.objects.all(),
+        'orders': Orders.objects.all(),
+        'order_details': OrderDetails.objects.all(),
+        'products': Products.objects.all(),
+        'shop_products': ShopProducts.objects.all(),
     }
     return render(request, 'adminpage.html', context)
