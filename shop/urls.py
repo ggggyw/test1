33c6daaf371fd44ba0fe86a1179401a90c2c4d19
Echozz66 <chen-zeyu-@outlook.com>
2024-04-12 10:06:39 +0800

    Update urls.py

--- shop/urls.py
+++ shop/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('shoppage/manage_products/delete_product/<int:product_id>/', views.delete_product, name='delete_product'),
     path('shoppage/manage_products/product_detail/<int:product_id>/', views.product_detail, name='product_detail'),
     path('shoppage/shop_order/', views.shop_order, name='shop_order'),
-    path('shoppage/sales_analysis/', views.sales_analysis, name='sales_analysis'),
+    path('shoppage/rfm/', views.rfm_analysis, name='rfm_analysis'),
     path('shoppage/shop_productdetails/<int:p_id>/', views.shop_productdetails, name='shop_productdetails'),
 ]
